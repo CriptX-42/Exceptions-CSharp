0d8027d9b3f9e804dd4f8e996365cbb5e11d03c4
Ricardo Sousa <ricardo-svc@live.com>
2018-12-27 14:07:18 -0200

    Customer Exception
    Initial

---
+++ CustomException/Entities/Reservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomException.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime ChekeIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime chekeIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            ChekeIn = chekeIn;
+            CheckOut = checkOut;
+        }
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(ChekeIn);
+            return (int)duration.TotalDays;
+        }
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            checkIn = ChekeIn;
+            CheckOut = CheckOut;
+        }
+        public override string ToString()
+        {
+            return "Room"
+                + RoomNumber
+                + ", Check-in"
+                + ChekeIn.ToString("dd/MM/yyyy")
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nights";
+        }
+    }
+}
---
+++ CustomException/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Room Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+        }
+    }
+}
