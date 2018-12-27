4a5b6d9c69067c60d96ccc8ef2ebea9af07ed65b
Ricardo Sousa <ricardo-svc@live.com>
2018-12-27 12:00:54 -0200

    Exception
    Try / Catch

---
+++ Exception/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Ops! Don't Panic " + e.Message);
+                Console.ReadLine();
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Ops! Don't Panic " + e.Message);
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
